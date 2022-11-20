d69192a9cf8761d5caeff583a2ed7e15b769e02c
abrikosmna <nik.mokijchuk@bk.ru>
2022-11-20 16:22:52 +0700

    20.11

---
+++ lesson11.py
@@ -0,0 +1,30 @@
+#import random
+#import datetime
+#while True:
+  #  number = input("Сколько дней мы генерим?До 70:")
+ #   if not number.isdigit() or int(number) > 70 or int(number) < 2:
+  #      print("Пошёл в жопу")
+  #      print("nigga " * 10)
+  #  else:
+  #      number = int(number)
+  #      break
+#birthdays = []
+#startOfYear = datetime.date(2022,1,1)
+#for _ in range(number):
+  #  shiftOfDays = datetime.timedelta(random.randint(0, 364))# сдвиг дней
+   # birthday = startOfYear + shiftOfDays
+   # birthdays.append(birthday)
+#for index in range(number):
+ #   print(f"{index +1}) {birthdays[index]}")
+#print("=" * 10)
+#for i in set(birthdays):
+
+  #  if birthdays.count(i) > 1:
+    #    print(f"- {i.strftime('%d, %m, %y')} встречаются {birthdays.count(i)} раз(а)")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+should_end = False
+white not should_end:
+    text = input("Введи сообщение")
+
+
--- pyko.py
+++ pyko.py
@@ -1,19 +1,43 @@
-import random]
+import random
 life = 10
 length = 3
-answer random.randint(100,999)
+answer = random.randint(100,999)
 answer = str(answer)
+
 while life:
     is_guessed = False
     print ("=" * 10)
-    print("Жизней:", life)
+
+    print("Жизней:", end = "")
+    for _ in range(life):
+        print("❤", end="")
+    print()
     guess = input("Предположение:")
-    if len(guess) != length or guess.isdigit():
+    if len(guess) != length or not guess.isdigit():
         print("Число от 100 до 999, позязя!!1!!111!!!!")
         continue
 
-
-
+    if guess == answer:
+        print("УРА ПОБЕДА!!!🏆❤❤🤪")
+        is_guessed = True
+        break
+    if is_guessed == False:
+        for i in range(length):
+            if guess [i] == answer[i]:
+                print("Fermi 🤩")
+                is_guessed = True
+                break
+    if is_guessed == False:
+        for char in guess:
+            if char in answer:
+                print("pico🥵")
+                is_guessed = True
+                break
+    if is_guessed == False:
+        print("Bagels😡😡😡👹")
+    life -= 1
+    if life == 0:
+        print("Проиграл!!!Число:",answer)
 
 
 
